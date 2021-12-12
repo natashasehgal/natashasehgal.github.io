e564547ff469c8211a96abdfaf0664a62459933b
Natasha Sehgal <nsehgal@siftscience.com>
2021-12-12 00:27:59 -0800

    basic nav

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,62 @@
+import { FaBars } from 'react-icons/fa';
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+background: black;
+height: 85px;
+display: flex;
+z-index: 12;
+`;
+
+export const NavLink = styled(Link)`
+color: white;
+display: flex;
+align-items: center;
+text-decoration: none;
+padding: 0 1rem;
+height: 100%;
+cursor: pointer;
+&.active {
+	color: red;
+}
+`;
+
+export const Bars = styled(FaBars)`
+display: left;
+`;
+
+export const NavMenu = styled.div`
+display: flex;
+align-items: left;
+`;
+
+export const NavBtn = styled.nav`
+display: flex;
+align-items: left;
+margin-right: 0px;
+/* Third Nav */
+/* justify-content: flex-end;
+width: 100vw; */
+@media screen and (max-width: 768px) {
+	display: none;
+}
+`;
+
+export const NavBtnLink = styled(Link)`
+border-radius: 4px;
+padding: 10px 22px;
+color: #000000;
+outline: none;
+border: none;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+text-decoration: none;
+/* Second Nav */
+margin-left: 24px;
+&:hover {
+	transition: all 0.2s ease-in-out;
+	background: red;
+	color: #808080;
+}
+`;
---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from 'react';
+  
+const About = () => {
+    return <div><h2>About Me - I'm a software engineer based in the Bay Area.</h2></div>;
+};
+  
+export default About;
